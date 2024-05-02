9c443345aff0bc833ca91e1e83accd0cf85c72c2
Sharifjon Abdullaev <GeorgeWash2018@gmail.com>
2024-05-02 19:11:33 +0500

    Repository interfaces added to Shared project

---
+++ src/Shared/Interfaces/Repository/IRepository.cs
@@ -0,0 +1,27 @@
+﻿/* 
+	YouTuber
+	Copyright (c) 2024, Sharifjon Abdulloev.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 3.0, 
+	as published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License, version 3.0, for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace Shared.Interfaces.Repository
+{
+	/// <summary>
+	/// Represents repository interface.
+	/// </summary>
+	/// <typeparam name="TEntity">The entity type this repository manages.</typeparam>
+	public interface IRepository<TEntity> : IReadOnlyRepository<TEntity>, IWriteOnlyRepository<TEntity> where TEntity : class
+	{
+	}
+}
